4dfe552319a5834db9cd7ebf2a0d19ac0e44f0b4
jays-commit <engine123qazqwe>
2022-01-09 18:53:08 +0000

    Play again loop

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,20 @@
 import json
 import difflib
+
+while True:
+
+    word = input("Enter Word: ")
+    if word == "/quit":
+        break
+
+    # print(translate(word))
+
+    cont = input("\nWould you like to continue? Y/N\n")
+    cont = cont.lower()
+    if cont == "y":
+        continue
+
+    if cont == "n":
+        break
+
+print("Session has ended")
