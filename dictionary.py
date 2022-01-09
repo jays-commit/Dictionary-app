1d633de8b3c238decfec27ae50942b6923523a85
jays-commit <engine123qazqwe>
2022-01-09 19:06:59 +0000

    tanslate function added

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,39 @@
 import json
 import difflib
 
+data = json.load(open("data.json"))
+
+
+def translate(word):
+    """
+    
+    :param word:
+    :return:
+    """
+    word = word.lower()
+    if word in data:
+        new_word = "".join(data[word])
+        return new_word
+    else:
+        suggest = difflib.get_close_matches(word, data.keys(), n=1)
+        new_suggest = "".join(suggest)
+
+        user_res = input("Did you mean {} instead? Enter Y if yes and" \
+                         " N if no ".format(new_suggest))
+        user_res = user_res.lower()
+        if user_res == "y":
+            return "".join(data[new_suggest])
+
+        return "Word doesn't exist please double check spelling"
+
+
 while True:
 
     word = input("Enter Word: ")
     if word == "/quit":
         break
 
-    # print(translate(word))
+    print(translate(word))
 
     cont = input("\nWould you like to continue? Y/N\n")
     cont = cont.lower()
